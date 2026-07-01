b2275345e7865707f07fc758007f4170f7654817
GrigoriySZ <zaitsevgs@gmail.com>
2026-07-01 21:22:37 +0700

    feat: design catalog with useMemo and complete props-drilling setup

---
+++ src/components/ActionButton.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface ActionButtonProps{
+    onAction: () => void;
+    label: string;
+}
+
+export const ActionButton: React.FC<ActionButtonProps> = ({ onAction, label }) => {
+    return (
+        <button
+            onClick={(e) => {
+                e.stopPropagation();
+                onAction();
+            }}
+        >
+            {label}
+        </button>
+    );
+};
