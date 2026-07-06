61607ea9837a3a96e0d77eb078accd111d886b17
GrigoriySZ <zaitsevgs@gmail.com>
2026-07-06 18:37:37 +0700

    feat: design catalog with useMemo and complete props-drilling setup

--- src/hooks/useClickOutside.ts
+++ src/hooks/useClickOutside.ts
@@ -1,4 +1,4 @@
-import React, { useEffect, RefObject } from "react";
+import { useEffect, RefObject } from "react";
 
 export function useClickOutside(ref: RefObject<HTMLElement>, handler: () => void) {
     useEffect(() => {
